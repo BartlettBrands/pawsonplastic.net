c4751970224f674add86367817bdd102657ffabe
Nathan Akers <ncakers@gmail.com>
2019-12-21 23:35:40 -0500

    i'm bad at small changes. this has whole new calculation variables and all mobile parts touched

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ var fonts = new FontFaceObserver('titling-gothic-fb-extended', {
     weight: 700
 });
 
+
+
 fonts.load().then(function () {
     console.log('Font is available');
     Marquee3k.init();
